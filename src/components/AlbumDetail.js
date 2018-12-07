17079d9f5969131cc4c771656adc2fbc8ec51763
Shashank reddy Yata <shashankreddyyata@shashanks-mbp.lan>
2018-12-07 17:17:57 -0600

    Added Buy now option

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Linking } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
+import Button from "./Button";
 
 const AlbumDetail = ({ albumData }) => {
-  const { title, artist, thumbnail_image, image } = albumData;
+  const { title, artist, thumbnail_image, image, url } = albumData;
   const {
     textContainer,
     thumbnailStyle,
@@ -25,6 +26,15 @@ const AlbumDetail = ({ albumData }) => {
       </CardSection>
       <CardSection>
         <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button
+          onPress={() => {
+            Linking.openURL(url);
+          }}
+        >
+          Buy now...!
+        </Button>
       </CardSection>
     </Card>
   );
